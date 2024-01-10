b3f98418cd8b4e354122afadf40c519232718171
Artem Yushkov <artem.yushkov2012@mail.ru>
2024-01-10 10:29:52 +0700

    Итог 2 лабы

--- bot.py
+++ bot.py
@@ -1,76 +1,70 @@
 import pandas as pd
 from aiogram import Bot, types, Dispatcher
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import json
-import logging
-import io
-import tempfile
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 API_TOKEN = json.load(open("config.json"))["num"]
-storage = MemoryStorage()
+
 bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-dp.middleware.setup(LoggingMiddleware())
 
-file_checked = False
 
-class AddExcelStates(StatesGroup):
-    WaitingForFile = State()
+class AddExcel(StatesGroup):
+    WaitingForDocument = State()
+
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer("Привет! Я бот, созданный для выдачи информации по группе студентов\nМне нужен файл Excel")
-    await message.answer("Это можно сделать через команду /addExcel")
+    await message.answer(
+        "Привет! Я бот созданный для выдачи информации по группе студенте\nДля начала введите /addExcel 'номер группы'. Например /addExcel ПИ101")
+
 
 @dp.message_handler(commands=['addExcel'])
 async def add_excel(message: types.Message):
-    global file_checked
-    await message.answer("Пожалуйста, отправьте файл Excel для добавления")
-    await AddExcelStates.WaitingForFile.set()
+    global group_id
+    command_parts = message.text.split()
+    if len(command_parts) == 2:
+        group_id = message.text.split(' ', 1)[1]
+        await message.reply("Теперь отправьте файл Excel")
+        await AddExcel.WaitingForDocument.set()
+    else:
+        await message.reply("Неверный формат команды. Введите /addExcel 'номер группы'. Например /addExcel ПИ101")
 
-@dp.message_handler(state=AddExcelStates.WaitingForFile, content_types=types.ContentType.DOCUMENT)
-async def process_file(message: types.Message, state: FSMContext):
+
+@dp.message_handler(content_types=types.ContentType.DOCUMENT, state=AddExcel.WaitingForDocument)
+async def process_document(message: types.Message, state: FSMContext):
+    document_id = message.document.file_id
+    file_info = await bot.get_file(document_id)
+    fi = file_info.file_path
     try:
-        file_info = message.document
-        file_id = file_info.file_id
+        await message.reply("Обработка...")
+        df_raw = pd.read_excel(f'https://api.telegram.org/file/bot{API_TOKEN}/{fi}')
+        df = pd.DataFrame(df_raw)
 
-        # Изменено: используем bot.download_file_by_id
-        file_content = await bot.download_file_by_id(file_id)
+        pi101_row = df[df['Группа'] == group_id]
+        unique_student_row = pi101_row['Личный номер студента'].unique()
+        unique_student_row_str = [str(number) for number in unique_student_row]
+        unique_student_row_str = ', '.join(unique_student_row_str)
+        year_control = pi101_row['Год'].unique()
+        year_control_row_str = [str(number) for number in year_control]
+        year_control_row_str = ', '.join(year_control_row_str)
+        type_control = pi101_row['Уровень контроля'].unique()
+        type_control_str = [str(number) for number in type_control]
+        type_control_str = ', '.join(type_control_str)
 
-        logger.info("Файл успешно получен и обработан.")
-
-        # Создаем временный файл и записываем в него содержимое BytesIO
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_file:
-            temp_file.write(file_content.getvalue())
-
-        # Читаем данные из временного файла
-        df = pd.read_excel(temp_file.name)
-
-        columns_to_check = ['Оценка', 'Сокращенная оценка', 'Период', 'Год', 'Семестр/Триместр', 'Курс', 'Часть года', 'Уровень контроля', 'Дисциплина', 'Личный номер студента', 'Группа', 'Факультет', 'Программа', 'Форма обучения', 'Тип финансирования']
-        missing_columns = set(columns_to_check) - set(df.columns)
-
-        if missing_columns:
-            await message.reply(f"Отсутствующие столбцы: {', '.join(missing_columns)}")
-        else:
-            await message.reply("Файл соответствует ожидаемым столбцам!")
-            file_checked = True
-
-
-        await message.reply("Файл успешно обработан!")
-        file_checked = True
+        await message.reply(
+            f'В исходном датасете содержалось {len(df)} оценок, из них {len(pi101_row)}\n оценок относятся к группе ПИ101\n В датасете находятся оценки {len(unique_student_row)} студентов со следующими личными номерами: {unique_student_row_str} \nИспользуемые формы контроля: {type_control_str}\nДанные представлены по следующим учебным годам: {year_control_row_str}')
     except Exception as e:
-        logger.error(f"Произошла ошибка при чтении файла: {e}")
-        await message.reply(f"Произошла ошибка при чтении файла: {e}")
-    finally:
-        await state.finish()
+        await message.reply(f"read error: {e}")
+    await state.finish()
+
 
 if __name__ == '__main__':
     from aiogram import executor
+
     executor.start_polling(dp, skip_updates=True)
+
